6be09a24ff94509bd4028eaf124e52acb6883599
ypinchina <batmanyp@foxmail.com>
2023-09-07 23:31:20 +0800

    笔记更新

--- hooks/src/2.useEffect副作用.js
+++ hooks/src/2.useEffect副作用.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react"
 function App () {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('yip')
   useEffect(() => {
-    // 每次重新渲染组件 副作用都会重新执行一次
+    console.log('副作用又执行了')
     document.title = count
-  })
+    console.log(name)
+  }, [count, name])
   return (
     <div className="App">
       <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={() => setName('myq')}>{name}</button>
     </div>
   )
 }
