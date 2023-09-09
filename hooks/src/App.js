fb18ab3951091b7d8a814641813c92dc577951ed
ypinchina <batmanyp@foxmail.com>
2023-09-09 23:22:38 +0800

    更新笔记

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,8 +1,7 @@
-import useWindowScroll from './3.useWindowScroll'
+
 function App () {
-  const y = useWindowScroll()
   return (
-    <div style={{ height: '12000px' }}>{y}</div>
+    <div ></div>
   )
 }
 
