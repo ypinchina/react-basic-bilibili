1e849c19cf6532c368788639a0b38ee6dfd21d27
ypinchina <batmanyp@foxmail.com>
2023-09-21 21:22:14 +0800

    提交更新

--- react-pc/src/App.js
+++ react-pc/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import AuthRoute from './components/AuthRoute'
+import './App.css'
 
 function App () {
   return (
